f3bf3ce57e48d5b36e6e1ef425ac828e2313f75b
Teresa Heidt <teresaheidt@hotmail.com>
2020-11-17 11:10:32 -0600

    updating nave bar

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         <Header />
     <Main />
     <Wrapper >
-      <Route exact path="/" component={About} />
+      <Route exact path="/about" component={About} />
       <Route exact path="/about" component={About} />
       <Route exact path="/contact" component={Contact} />
       <Route exact path="/projects" component={Projects} />
