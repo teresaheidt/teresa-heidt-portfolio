896771fa559c441b0b5d411d9e6d5a76309f4e62
Teresa Heidt <teresaheidt@hotmail.com>
2020-11-18 13:33:24 -0600

    error in useContext

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,36 @@
 import React from "react";
-import Container from "../components/Container";
+import Fade from 'react-reveal/Fade';
+import { Container } from 'react-bootstrap';
+import PortfolioContext from '../context/context';
 
-function Contact() {
+
+
+const Contact = () => {
+    const { contact } = useContext(PortfolioContext);
+    const { cta, btn, email } = contact;
+  
     return (
-        <div>Contact me</div>
-     
+      <section id="contact">
+        <Container>
+          {/* <Title title="Contact" /> */}
+          <Fade bottom duration={1000} delay={800} distance="30px">
+            <div className="contact-wrapper">
+              <p className="contact-wrapper__text">
+                {cta || 'Would you like to work with me? Awesome!'}
+              </p>
+              <a
+                target="_blank"
+                rel="teresaheidt@hotmail.com"
+                className="cta-btn cta-btn--resume"
+                href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
+              >
+                {btn || "Let's Talk"}
+              </a>
+            </div>
+          </Fade>
+        </Container>
+      </section>
     );
-}
-
-export default Contact;
+  };
+  
+  export default Contact;
